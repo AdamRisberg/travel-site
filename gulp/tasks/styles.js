61e7268d3008734195b41b1e49b0abfc0977fca0
Adam Risberg <myniztan@hotmail.com>
2018-03-26 04:26:18 -0500

    Completed header CSS

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,10 +6,11 @@ var nested = require("postcss-nested");
 var cssImport = require("postcss-import");
 var mixins = require("postcss-mixins");
 var perfectionist = require("perfectionist")({ indentChar: "\t", indentSize: 1 });
+var hexrgba = require("postcss-hexrgba");
 
 gulp.task("styles", function () {
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer, perfectionist]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer, perfectionist]))
     .on("error", function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit("end");
